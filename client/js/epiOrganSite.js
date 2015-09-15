dff61c4b7a130968f3e4fd72fc256fd854affd58
Andy Shapiro <shapiromatron@gmail.com>
2015-09-15 12:21:26 +0200

    first organ-site filter implementation

--- client/js/epiOrganSite.js
+++ client/js/epiOrganSite.js
@@ -18,18 +18,52 @@ Template.epiOrganSiteMain.helpers(_.extend({
       });
 
       return results;
-    },
-    showPlots: function() {
-      return Session.get("epiRiskShowPlots");
     }
   }, clientShared.abstractMainHelpers));
 Template.epiOrganSiteMain.events({
   'change #organSiteSelector': function(evt, tmpl) {
     tmpl.organSites.set($(evt.target).val() || []);
     return clientShared.toggleRiskPlot();
+  },
+  'click #selectVisible': function(evt, tmpl) {
+    Session.set("eosShowCheckbox", !Session.get("eosShowCheckbox"));
   }
 });
 Template.epiOrganSiteMain.onCreated(function() {
   this.subscribe('epiCollective', this.data.volumeNumber, this.data.monographAgent);
   this.organSites = new ReactiveVar([]);
+  Session.setDefault("eosShowCheckbox", false);
+  Session.setDefault("epiRiskShowPlots", false);
 });
+
+
+var trHelpers = {
+  showCheckbox: function(){
+    return Session.get("eosShowCheckbox");
+  },
+  hideRow: function(){
+    return (!Session.get("eosShowCheckbox") && Template.instance().rowHidden.get()) ? "hidden" : "";
+  },
+  rowVisible: function(){
+    return Template.instance().rowHidden.get() ? "" : "checked";
+  },
+  showPlots: function() {
+    return Session.get("epiRiskShowPlots");
+  }
+}, trEvents = {
+  "click .hideRow" : function(evt, tmpl){
+    tmpl.rowHidden.set(!tmpl.rowHidden.get());
+    console.log(tmpl.rowHidden.get())
+  }
+}, trCreated = function(){
+  this.rowHidden = new ReactiveVar(false);
+}
+
+Template.epiOrganSiteFullTr.helpers(trHelpers);
+Template.epiOrganSiteFullTr.events(trEvents);
+Template.epiOrganSiteFullTr.onCreated(trCreated);
+
+
+Template.epiOrganSiteResultTr.helpers(trHelpers);
+Template.epiOrganSiteResultTr.events(trEvents);
+Template.epiOrganSiteResultTr.onCreated(trCreated);
