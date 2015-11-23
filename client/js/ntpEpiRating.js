418dcdc6364050827fab39dc69b5c72ada2c8d82
Andy Shapiro <shapiromatron@gmail.com>
2015-11-23 17:33:17 -0500

    remove extra session variables from ntp-epi rating views

--- client/js/ntpEpiRating.js
+++ client/js/ntpEpiRating.js
@@ -1,18 +1,10 @@
 Template.ntpEpiRatingMain.helpers(clientShared.abstractMainHelpers);
 Template.ntpEpiRatingMain.onCreated(function() {
     Session.set('evidenceType', 'ntpEpiDescriptive');
-    Session.set('evidenceShowNew', false);
-    Session.set('evidenceShowAll', false);
-    Session.set('evidenceEditingId', null);
-    Session.set('nestedEvidenceEditingId', null);
     this.subscribe('ntpEpiDescriptive', Session.get('Tbl')._id);
 });
 Template.ntpEpiRatingMain.onDestroyed(function() {
     Session.set('evidenceType', null);
-    Session.set('evidenceShowNew', false);
-    Session.set('evidenceShowAll', false);
-    Session.set('evidenceEditingId', null);
-    Session.set('nestedEvidenceEditingId', null);
 });
 
 
