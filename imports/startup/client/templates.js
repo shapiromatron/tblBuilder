bc84d363a1aa9e0c7ddb18f3b31f1b5a70320d3c
Andy Shapiro <shapiromatron@gmail.com>
2016-04-13 17:39:13 -0400

    simplify client/main

---
+++ imports/startup/client/templates.js
@@ -0,0 +1,6 @@
+import { Template } from 'meteor/templating';
+
+
+tblBuilderCollections.evidenceLookup['epiDescriptive'].nested_template = Template.epiResultForm;
+tblBuilderCollections.evidenceLookup['animalEvidence'].nested_template = Template.animalEndpointForm;
+tblBuilderCollections.evidenceLookup['ntpEpiDescriptive'].nested_template = Template.ntpEpiResultForm;
