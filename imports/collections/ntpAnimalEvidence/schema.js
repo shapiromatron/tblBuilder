4a3f15bb55dbe5d31750a93465b596ddd36ac86e
Andy Shapiro <shapiromatron@gmail.com>
2016-05-26 12:15:26 -0400

    add additional references

--- imports/collections/ntpAnimalEvidence/schema.js
+++ imports/collections/ntpAnimalEvidence/schema.js
@@ -4,10 +4,15 @@ import {
 
 
 export default {
-    // #1: General
+    // #1: General information
     referenceID: {
         label: 'Reference',
         type: SimpleSchema.RegEx.Id,
     },
+    additionalReferences: {
+        label: 'References',
+        type: [SimpleSchema.RegEx.Id],
+        minCount: 0,
+        popoverText: 'References of earlier updates or related publications',
     },
 };
