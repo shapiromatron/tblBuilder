2160d9482eb6e96d09a52c3efd6f7b45d7320b03
Andy Shapiro <shapiromatron@gmail.com>
2017-05-17 11:13:12 -0400

    revise ntp template

--- private/scripts/reports/animal/ntp_bias.py
+++ private/scripts/reports/animal/ntp_bias.py
@@ -17,7 +17,7 @@ class NtpAnimalBias(DOCXReport):
     def build_tbl(self, study):
         colWidths = (2, 4.5)
         tbl = TableMaker(colWidths, numHeaders=1, firstRowCaption=False,
-                         tblStyle="ntpTbl")
+                         tblStyle='ntpTbl')
 
         # Table caption
         txt = u'Table X: {} ({} {}): {}: {}'.format(
@@ -30,8 +30,8 @@ class NtpAnimalBias(DOCXReport):
         self.doc.add_heading(txt, level=2)
 
         # write header
-        tbl.new_th(0, 0, 'Study utility domain\nQuestion')
-        tbl.new_th(0, 1, 'Rating\nRationale')
+        tbl.new_th(0, 0, 'Study utility domain and question')
+        tbl.new_th(0, 1, 'Rating and rationale')
 
         # study design section
         self.add_rr_header(tbl, 1, 'Study design')
@@ -50,105 +50,105 @@ class NtpAnimalBias(DOCXReport):
         response = 'Yes' if study['historicalDataAvailable'] is True else 'No'
         tbl.new_td_txt(4, 1, response)
 
-        # exposure section
-        self.add_rr_header(tbl, 5, 'Exposure')
-
         self.add_rr_row(
-            tbl, 6, 'Chemical characterization',
-            study['chemicalCharacterizationRating'],
-            study['chemicalCharacterizationRationale'])
-
-        self.add_rr_row(
-            tbl, 7, 'Dosing regimen',
-            study['dosingRegimenRating'],
-            study['dosingRegimenRationale'])
-
-        # outcome section
-        self.add_rr_header(tbl, 8, 'Outcome')
-
-        self.add_rr_row(
-            tbl, 9, 'Pathology',
-            study['outcomeMethodsRating'],
-            study['outcomeMethodsRationale'])
-
-        self.add_rr_row(
-            tbl, 10, 'Consistency between groups',
-            study['groupConsistencyRating'],
-            study['groupConsistencyRationale'])
-
-        # confounding section
-        self.add_rr_header(tbl, 11, 'Confounding')
-
-        self.add_rr_row(
-            tbl, 12, 'Confounding',
-            study['confoundingRating'],
-            study['confoundingRationale'])
-
-        # reporting & analysis section
-        self.add_rr_header(tbl, 13, 'Reporting and analysis')
-
-        self.add_rr_row(
-            tbl, 14, 'Reporting data and statistics',
-            study['statisticalReportingRating'],
-            study['statisticalReportingRationale'])
-
-        self.add_rr_row(
-            tbl, 15, 'Combining lesions',
-            study['tumorCombiningRating'],
-            study['tumorCombiningRationale'])
-
-        # sensitivity section
-        self.add_rr_header(tbl, 16, 'Sensitivity')
-
-        self.add_rr_row(
-            tbl, 17, 'Animal model',
+            tbl, 5, 'Animal model (sensitivity)',
             study['animalModelSensitivityRating'],
             study['animalModelSensitivityRationale'])
 
         self.add_rr_row(
-            tbl, 18, 'Statistical power',
+            tbl, 6, 'Statistical power (sensitivity)',
             study['statisticalPowerRating'],
             study['statisticalPowerRationale'])
 
+        # exposure section
+        self.add_rr_header(tbl, 7, 'Exposure')
+
         self.add_rr_row(
-            tbl, 19, 'Exposure duration',
+            tbl, 8, 'Chemical characterization',
+            study['chemicalCharacterizationRating'],
+            study['chemicalCharacterizationRationale'])
+
+        self.add_rr_row(
+            tbl, 9, 'Dosing regimen',
+            study['dosingRegimenRating'],
+            study['dosingRegimenRationale'])
+
+        self.add_rr_row(
+            tbl, 10, 'Exposure duration (sensitivity)',
             study['exposureDurationRating'],
             study['exposureDurationRationale'])
 
         self.add_rr_row(
-            tbl, 20, 'Dose/response',
+            tbl, 11, 'Dose-response (sensitivity)',
             study['doseResponseRating'],
             study['doseResponseRationale'])
 
+        # outcome section
+        self.add_rr_header(tbl, 12, 'Outcome')
+
         self.add_rr_row(
-            tbl, 21, 'Study duration',
+            tbl, 13, 'Pathology',
+            study['outcomeMethodsRating'],
+            study['outcomeMethodsRationale'])
+
+        self.add_rr_row(
+            tbl, 14, 'Consistency between groups',
+            study['groupConsistencyRating'],
+            study['groupConsistencyRationale'])
+
+        self.add_rr_row(
+            tbl, 15, 'Study duration (sensitivity)',
             study['durationRating'],
             study['durationRationale'])
+
+        # confounding section
+        self.add_rr_header(tbl, 16, 'Confounding')
+
+        self.add_rr_row(
+            tbl, 17, 'Confounding',
+            study['confoundingRating'],
+            study['confoundingRationale'])
+
+        # reporting & analysis section
+        self.add_rr_header(tbl, 18, 'Reporting and analysis')
+
+        self.add_rr_row(
+            tbl, 19, 'Reporting data and statistics',
+            study['statisticalReportingRating'],
+            study['statisticalReportingRationale'])
+
+        self.add_rr_row(
+            tbl, 20, 'Combining lesions',
+            study['tumorCombiningRating'],
+            study['tumorCombiningRationale'])
 
         # render table
         tbl.render(self.doc)
 
         # overall utility
-        txt = u'Overall utility: {}\n{}'.format(
+        txt = u' {}. {}'.format(
             study['overallUtility'],
             study['overallUtilityRationale']
         )
-        self.doc.add_paragraph(txt)
+        p = self.doc.add_paragraph('')
+        p.add_run('Overall utility: ').bold = True
+        p.add_run(txt)
+
         self.doc.add_page_break()
 
     def create_content(self):
         doc = self.doc
         d = self.context
 
-        txt = u"{} {}: potential bias".format(
-            d["table"]["volumeNumber"],
-            d["table"]["monographAgent"],
+        txt = u'{} {}: potential bias'.format(
+            d['table']['volumeNumber'],
+            d['table']['monographAgent'],
         )
         doc.add_heading(txt, level=1)
-        doc.add_paragraph(d["table"]["name"])
+        doc.add_paragraph(d['table']['name'])
 
         for study in d['studies']:
             self.build_tbl(study)
 
     def get_template_fn(self):
-        return "base.docx"
+        return 'base.docx'
