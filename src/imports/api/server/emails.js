36b98f6f4b5f577ef07dbe42c72593147fe1f431
Andy Shapiro <shapiromatron@gmail.com>
2017-06-01 14:11:43 -0400

    update emails

--- src/imports/api/server/emails.js
+++ src/imports/api/server/emails.js
@@ -1,26 +1,51 @@
 import { Accounts } from 'meteor/accounts-base';
+import {Meteor} from 'meteor/meteor';
+
+
+let context = Meteor.settings.public.context.toUpperCase(),
+    from_email = Meteor.settings.from_email;
 
 // Global email configuration settings
-Accounts.emailTemplates.siteName = 'IARC Table Maker';
+Accounts.emailTemplates.siteName = `${context} Table Builder`;
 
 
 // sendEnrollmentEmail
-Accounts.emailTemplates.from = 'IARC Table Maker Admin <admin@iarcTables.com>';
+Accounts.emailTemplates.from = `${context} Table Builder Admin<${from_email}>`;
 Accounts.emailTemplates.enrollAccount.subject = function() {
-    return 'You\'ve been granted access to IARC Table Maker';
+    return `You've been granted access to the ${context} Table Builder`;
 };
-
 Accounts.emailTemplates.enrollAccount.text = function(user, url) {
     var greeting;
-    greeting = (user.profile != null) && (user.profile.fullName != null) ? 'Hello ' + user.profile.fullName : 'Hello';
-    return greeting + ', \n\nA site administrator has created an account for you to contribute content to the IARC table maker software. This web-application is designed to store key data for decisions in the IARC process.\n\nTo activate your account, click the link below:\n\n' + url + '\n\nThank you, \nThe IARC Table Maker Team';
+    greeting = (user.profile != null) && (user.profile.fullName != null) ?
+        'Hello ' + user.profile.fullName : 'Hello';
+    return `${greeting},
+
+A site administrator has created an account for you to contribute content to the ${context} table maker.
+This web-application is designed to store key data for decisions in the monographs process.
+
+To activate your account, click the link below:
+${url}
+
+Thank you,
+The ${context} table maker team`;
 };
 
 // sendResetPasswordEmail
 Accounts.emailTemplates.resetPassword.subject = function() {
-    return 'Reset your password at IARC Table Maker';
+    return `Reset your password for the ${context} Table Builder`;
 };
 Accounts.emailTemplates.resetPassword.text = function(user, url) {
-    var greeting = (user.profile != null) && (user.profile.fullName != null) ? 'Hello ' + user.profile.fullName : 'Hello';
-    return greeting + ', \n\nWe\'ve been notified that you\'d like to reset your password. To reset your password, click the link below:\n\n' + url + '\n\nIf you do not want to reset your password, you can safely ignore this email.\n\nThank you, \nThe IARC Table Maker Team';
+    var greeting = (user.profile != null) && (user.profile.fullName != null) ?
+        'Hello ' + user.profile.fullName : 'Hello';
+    return `${greeting},
+
+We've been notified that you'd like to reset your password on ${Meteor.absoluteUrl()}.
+
+To reset your password, click the link below:
+${url}
+
+If you do not want to reset your password, you can safely ignore this email.
+
+Thank you,
+The ${context} Table Builder team`;
 };
