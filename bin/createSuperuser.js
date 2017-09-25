de6f1eb8ea6add718e28f01536859e0759253aac
JoshAddington <josh.addington@gmail.com>
2017-09-25 16:51:12 -0400

    fix adding user roles

--- bin/createSuperuser.js
+++ bin/createSuperuser.js
@@ -3,9 +3,10 @@ import { Roles } from 'meteor/alanning:roles';
 
 
 var user = Accounts.createUser({
-    username: 'username',
-    email: 'email',
-    password: 'password',
-});
+        username: 'username',
+        email: 'email',
+        password: 'password',
+    }),
+    role_names = Roles.getAllRoles().map((r) => r.name);
 
-Roles.setUserRoles(user, ['default', 'staff', 'superuser']);
+Roles.setUserRoles(user, role_names);
