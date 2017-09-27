e1d529322cc315ab9382dab726d0d253cac0f5ac
Andy Shapiro <shapiromatron@gmail.com>
2017-09-27 15:41:43 -0400

    move development notes to readme

--- bin/createSuperuser.js
+++ bin/createSuperuser.js
@@ -3,9 +3,9 @@ import { Roles } from 'meteor/alanning:roles';
 
 
 var user = Accounts.createUser({
-        username: 'username',
-        email: 'email',
-        password: 'password',
+        username: 'myusername',
+        email: 'myemail@email.com',
+        password: 'mypassword',
     }),
     role_names = Roles.getAllRoles().map((r) => r.name);
 
